c9ed7c37060372e2304bac3d78945e6c63dfe494
slwool <slwenoo@hotmail.com>
2020-01-07 15:02:58 +0800

    springboot-zookeeper

---
+++ test/java/com/slwool/springbootzookeeper/SpringbootZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slwool.springbootzookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootZookeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
